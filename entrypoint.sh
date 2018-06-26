fee43418986bd838acd19f8f75841303bb4c473b
Necati Şeker <37519860+necatiseker@users.noreply.github.com>
2018-06-26 16:30:10 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ EXECUTORS="${EXECUTORS:-8}"
 FSROOT="${FSROOT:-/tmp/jenkins}"
 
 mkdir -p $FSROOT
-java -jar swarm-client.jar -labels $LABELS -executors $EXECUTORS -fsroot $FSROOT \
+java -jar slave.jar -labels $LABELS -executors $EXECUTORS -fsroot $FSROOT \
   -name docker-$(hostname) -master $JENKINS_ADDR -username $JENKINS_USER -password $JENKINS_PASS
